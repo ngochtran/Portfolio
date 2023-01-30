4fa620caa4b5c6259c56728460494fdd07145cff
ngochtran <ngochtran2015@gmail.com>
2023-01-30 00:03:00 -0800

    art gallery

--- slideshow.js
+++ slideshow.js
@@ -38,6 +38,68 @@ function animate() {
     setTimeout("animate()", delay);
 }
 
+function showMe(value){
+    if (value=="selected-works"){
+        myArt = new Array();
+        for (var i = 0; i < 3; i++){
+            myArt[i] = new Image();
+            myArt[i].src = 'Images/sw' + (i + 1) + '.png';
+        }
+        document.getElementById('sw').style.display="block";
+        document.getElementById('il').style.display="none";
+        document.getElementById('ca').style.display="none";
+        document.getElementById('d3').style.display="none";
+        document.getElementById('sk').style.display="none";
+    }
+    if (value=="illust"){
+        myArt = new Array();
+        for (var i = 0; i < 6; i++){
+            myArt[i] = new Image();
+            myArt[i].src = 'Images/illust' + (i + 1) + '.png';
+        }
+        document.getElementById('sw').style.display="none";
+        document.getElementById('il').style.display="block";
+        document.getElementById('ca').style.display="none";
+        document.getElementById('d3').style.display="none";
+        document.getElementById('sk').style.display="none";
+    }
+    if (value=="concept-art"){
+        myArt = new Array();
+        for (var i = 0; i < 3; i++){
+            myArt[i] = new Image();
+            myArt[i].src = 'Images/ca' + (i + 1) + '.png';
+        }
+        document.getElementById('sw').style.display="none";
+        document.getElementById('il').style.display="none";
+        document.getElementById('ca').style.display="block";
+        document.getElementById('d3').style.display="none";
+        document.getElementById('sk').style.display="none";
+    }
+    if (value=="3D"){
+        myArt = new Array();
+        for (var i = 0; i < 3; i++){
+            myArt[i] = new Image();
+            myArt[i].src = 'Images/3d' + (i + 1) + '.png';
+        }
+        document.getElementById('sw').style.display="none";
+        document.getElementById('il').style.display="none";
+        document.getElementById('ca').style.display="none";
+        document.getElementById('d3').style.display="block";
+        document.getElementById('sk').style.display="none";
+    }
+    if (value=="sketches"){
+        myArt = new Array();
+        for (var i = 0; i < 3; i++){
+            myArt[i] = new Image();
+            myArt[i].src = 'Images/sketch' + (i + 1) + '.png';
+        }
+        document.getElementById('sw').style.display="none";
+        document.getElementById('il').style.display="none";
+        document.getElementById('ca').style.display="none";
+        document.getElementById('d3').style.display="none";
+        document.getElementById('sk').style.display="block";
+    }
+}
 function choseSW(totalArt){
     myArt = new Array();
     for (var i = 0; i < totalArt; i++){
